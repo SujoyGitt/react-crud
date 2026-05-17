be267f99376a933a9135063a3a7ad3f0a2862551
Sujoy Ghosal <sujoy143656@gmail.com>
2026-05-17 21:20:33 +0530

    Add Loader for  user better experience

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Layout from "./layout/Layout";
 import { ConfirmDialog } from "primereact/confirmdialog";
 import { Toast } from "primereact/toast";
 import { lazy, Suspense, useRef } from "react";
+import { ProgressSpinner } from "primereact/progressspinner";
+
 function App() {
   const toast = useRef<Toast | null>(null);
 
@@ -16,9 +18,46 @@ function App() {
       <ConfirmDialog />
       <Routes>
         <Route element={<Layout />}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Route path="/" element={<UsersList toast={toast} />} />
-          </Suspense>
+          <Route
+            path="/"
+            element={
+              <Suspense
+                fallback={
+                  <div
+                    style={{
+                      height: "100vh",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      flexDirection: "column",
+                      gap: "1rem",
+                      background: "#f8fafc",
+                    }}
+                  >
+                    <ProgressSpinner
+                      style={{ width: "60px", height: "60px" }}
+                      strokeWidth="4"
+                      animationDuration=".8s"
+                    />
+
+                    <h3
+                      style={{
+                        color: "#334155",
+                        fontWeight: 500,
+                        margin: 0,
+                      }}
+                    >
+                      Loading...
+                    </h3>
+                  </div>
+                }
+              >
+                <Routes>
+                  <Route path="/" element={<UsersList toast={toast} />} />
+                </Routes>
+              </Suspense>
+            }
+          />
           <Route path="/add" element={<AddEdit toast={toast} />} />
           <Route path="/edit/:id" element={<AddEdit toast={toast} />} />
         </Route>
